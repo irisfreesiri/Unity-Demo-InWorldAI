248d2bd82e3362722c17ade6109c75e6acb2c9e4
irisfreesiri <iris@vertexgames.cn>
2023-09-01 01:18:19 +0800

    fixed UPR error

--- Assets/Inworld.Assets/Scripts/InworldCameraController.cs
+++ Assets/Inworld.Assets/Scripts/InworldCameraController.cs
@@ -52,35 +52,37 @@ namespace Inworld.Sample
                 Cursor.lockState = Cursor.visible ? CursorLockMode.None : CursorLockMode.Locked;
             }
 
-            // Rotation
+            // IRIS-TODO: modify the operation of the camera
             if (Cursor.lockState == CursorLockMode.Locked)
             {
+                // Rotation
                 Vector2 mouseMovement = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y") * (invertY ? 1 : -1));
                 float mouseSensitivityFactor = mouseSensitivityCurve.Evaluate(mouseMovement.magnitude);
                 m_TargetCameraState.yaw += mouseMovement.x * mouseSensitivityFactor;
                 m_TargetCameraState.pitch += mouseMovement.y * mouseSensitivityFactor;
+
+                // Translation
+                Vector3 translation = GetInputTranslationDirection() * Time.deltaTime;
+
+                // Speed up movement when shift key held
+                if (Input.GetKey(KeyCode.LeftShift))
+                {
+                    translation *= 10.0f;
+                }
+
+                // Modify movement by a boost factor (defined in Inspector and modified in play mode through the mouse scroll wheel)
+                boost += Input.mouseScrollDelta.y * 0.2f;
+                translation *= Mathf.Pow(2.0f, boost);
+                m_TargetCameraState.Translate(translation);
+
+                // Framerate-independent interpolation
+                // Calculate the lerp amount, such that we get 99% of the way to our target in the specified time
+                float positionLerpPct = 1f - Mathf.Exp(Mathf.Log(1f - 0.99f) / positionLerpTime * Time.deltaTime);
+                float rotationLerpPct = 1f - Mathf.Exp(Mathf.Log(1f - 0.99f) / rotationLerpTime * Time.deltaTime);
+                m_InterpolatingCameraState.LerpTowards(m_TargetCameraState, positionLerpPct, rotationLerpPct);
+
+                m_InterpolatingCameraState.UpdateTransform(transform);
             }
-            // Translation
-            Vector3 translation = GetInputTranslationDirection() * Time.deltaTime;
-
-            // Speed up movement when shift key held
-            if (Input.GetKey(KeyCode.LeftShift))
-            {
-                translation *= 10.0f;
-            }
-
-            // Modify movement by a boost factor (defined in Inspector and modified in play mode through the mouse scroll wheel)
-            boost += Input.mouseScrollDelta.y * 0.2f;
-            translation *= Mathf.Pow(2.0f, boost);
-            m_TargetCameraState.Translate(translation);
-
-            // Framerate-independent interpolation
-            // Calculate the lerp amount, such that we get 99% of the way to our target in the specified time
-            float positionLerpPct = 1f - Mathf.Exp(Mathf.Log(1f - 0.99f) / positionLerpTime * Time.deltaTime);
-            float rotationLerpPct = 1f - Mathf.Exp(Mathf.Log(1f - 0.99f) / rotationLerpTime * Time.deltaTime);
-            m_InterpolatingCameraState.LerpTowards(m_TargetCameraState, positionLerpPct, rotationLerpPct);
-
-            m_InterpolatingCameraState.UpdateTransform(transform);
         }
 
         void OnEnable()
